ef6a4cdfa8619c358dbc249f92fd413fa8d1329b
Pavlo Karas <thepavlokaras@gmail.com>
2025-02-23 10:19:00 +0200

    lab5

---
+++ lib/lab5.ex
@@ -0,0 +1,22 @@
+defmodule Lab5 do
+  @remote :node2@nules
+
+  def main() do
+    IO.inspect(task(), label: "my machine")
+
+    true = Node.connect(@remote)
+    remote_machine = :rpc.call(@remote, Lab5, :task, [])
+
+    IO.inspect(remote_machine, label: "remote machine")
+  end
+
+  def task() do
+    matrix = Eigenvalue.random_matrix(5_000)
+
+    TaskTimer.timer(fn ->
+      TaskTimer.timer(fn ->
+        {_eigenvector, _eigenvalue} = Eigenvalue.power_iteration(matrix)
+      end)
+    end)
+  end
+end
